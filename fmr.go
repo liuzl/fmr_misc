90a3d9fede1dfc5d79775e76e084b26ccb01404b
Zhanliang Liu <zhanliangliu@qq.com>
2018-11-29 11:17:30 +0800

    fmr tool

--- fmr.go
+++ fmr.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/liuzl/fmr"
 )
@@ -18,5 +22,34 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(grammar)
+	br := bufio.NewReader(os.Stdin)
+	for {
+		line, c := br.ReadString('\n')
+		if c == io.EOF {
+			break
+		}
+		if c != nil {
+			log.Fatal(c)
+		}
+		line = strings.TrimSpace(line)
+		fmt.Println(line)
+
+		ps, err := grammar.EarleyParseMaxAll(line, "start", "number")
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i, p := range ps {
+			for _, f := range p.GetFinalStates() {
+				trees := p.GetTrees(f)
+				fmt.Printf("p%d tree number:%d\n", i, len(trees))
+				for _, tree := range trees {
+					sem, _ := tree.Semantic()
+					nl := tree.NL()
+					tag := p.Tag(f)
+					fmt.Printf("%s(%s) = %s\n", tag, nl, sem)
+				}
+			}
+		}
+
+	}
 }
